13f0e4edf5dda7b52132c989b93aacc6a5957538
thomas0124 <qingshuitouzhen@gmail.com>
2024-07-12 18:37:21 +0900

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'template',
-  description: 'template'
+  title: 'thomas’s portfolio',
+  description: 'トーマスのポートフォリオ'
 }
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import Image from 'next/image'
-import useEffect from 'react'
-
 export default function Home() {
-  return (
-    <div>
-      Test
-      <p>test2</p>
-    </div>
-  )
+  return <div>トーマスのポートフォリオ</div>
 }
