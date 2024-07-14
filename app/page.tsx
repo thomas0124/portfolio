008d2108b9c54b155d32bde43f80d94f5d73a5b7
thomas0124 <qingshuitouzhen@gmail.com>
2024-07-14 11:01:58 +0900

    add:Loadコンポーネント

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+'use client'
+
+import { useState, useEffect } from 'react'
 import Header from '@/components/Header'
+import LoadScreen from '@/components/LoadScreen'
 
 export default function Home() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 3000) // ローディング画面を3秒間表示
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) {
+    return <LoadScreen onComplete={() => setLoading(false)} />
+  }
   return <Header />
 }
